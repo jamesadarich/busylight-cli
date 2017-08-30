5290c8053733dcf2a6503b88a984d7c84e58e4b2
James Richford <jrichford@neworbit.co.uk>
2017-08-30 10:05:57 +0100

    add pulse and blink commands

--- src/busylight-cli.ts
+++ src/busylight-cli.ts
@@ -4,15 +4,29 @@ const light = busylight.get();
 
 const command = process.argv[2];
 
-if (command === "busy") {
-    light.light("red");
-}
-else if (command === "free") {
-    light.light("green");
-}
-else if (command === "police") {
+if (command === "police") {
     light.blink(["red", "blue"], 150);
 }
-else {
+else if (!command) {    
     light.blink(["red", "orange", "green", "blue", "purple"], 150);
+}
+else {
+    let color = command;
+
+    if (command === "busy") {
+        color = "red";
+    }
+    else if (command === "free") {
+        color = "green";
+    }
+
+    if (process.argv.indexOf("--pulse") > -1) {
+        light.pulse(color, 500);
+    }    
+    else if (process.argv.indexOf("--blink") > -1) {
+        light.blink(color, 500);
+    }
+    else {
+        light.light(color);
+    }
 }
